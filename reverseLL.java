2078acb2fd9b4989a99d9ef8660b1d927aef6487
akhikolla <akhilakollasrinu424jf@gmail.com>
2020-11-06 21:49:31 -0700

    reverse ll

---
+++ reverseLL.java
@@ -0,0 +1,14 @@
+Node* reverse(Node *head)
+{
+        Node* current = head;
+        Node *prev = NULL, *next = NULL;
+ 
+        while (current != NULL) {
+            next = current->next;
+            current->next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+}
+ 
